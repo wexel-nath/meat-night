b4505d70ffa873fd1c06227e3e284b12e90dbfca
Nathan Welch <nathanwelch_@hotmail.com>
2019-06-13 19:20:35 +1000

    Add 2nd dinner in initial values
    Add 2nd dinner in initial values

--- db/initial_values/03_guest.sql
+++ db/initial_values/03_guest.sql
@@ -1,7 +1,11 @@
 
-INSERT INTO guest (guest_id, dinner_id)
+INSERT INTO guest (dinner_id, guest_id)
 VALUES
   (1, 1),
+  (1, 2),
+  (1, 5),
   (2, 1),
-  (5, 1);
+  (2, 2),
+  (2, 4),
+  (2, 5);
 -- TODO: insert the rest from google drive
